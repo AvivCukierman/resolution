9613b84565264c0383ac13299a7aa2171380c532
AvivCukierman <acukierm@slac.stanford.edu>
2016-02-29 15:35:00 -0800

    Delete resolution_test.sh
    New sample run script.

--- analysis/resolution_test.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-python resolution.py --inputDir /nfs/slac/g/atlas/u01/users/acukierm/resolution/LSF_EM_JZ2_lowpt/fetch/data-outputTree -i myjets_unsub --maxeta 1 --mindr 0.6 --numEvents 10000 -r
