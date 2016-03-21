d6258a52e8e4291e81b2471359ceaba7cca8a48e
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-03-21 12:52:13 -0700

    Helper function for extracting quantiles from weighted distribution

---
+++ analysis/quantile.py
@@ -0,0 +1,30 @@
+#credit: https://github.com/nudomarinero/wquantiles
+
+import numpy as np
+
+def quantile(data,weights,quantile):
+    # Check the data
+    if not isinstance(data, np.matrix) :
+        data = np.asarray(data)
+    if not isinstance(weights, np.matrix) :
+        weights = np.asarray(weights)
+    nd = data.ndim
+    if nd != 1:
+        raise TypeError("data must be a one dimensional array")
+    ndw = weights.ndim
+    if ndw != 1:
+        raise TypeError("weights must be a one dimensional array")
+    if data.shape != weights.shape:
+        raise TypeError("the length of data and weights must be the same")
+    if ((quantile > 1.) or (quantile < 0.)):
+        raise ValueError("quantile must have a value between 0. and 1.")
+    # Sort the data
+    ind_sorted = np.argsort(data)
+    sorted_data = data[ind_sorted]
+    sorted_weights = weights[ind_sorted]
+    # Compute the auxiliary arrays
+    Sn = np.cumsum(sorted_weights)
+    # TODO: Check that the weights do not sum zero
+    Pn = (Sn-0.5*sorted_weights)/np.sum(sorted_weights)
+    # Get the value of the weighted median
+    return np.interp(quantile, Pn, sorted_data)
