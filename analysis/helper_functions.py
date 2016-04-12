cceea24888821efda93f9a2fbb9869910b6e2237
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-04-11 18:15:07 -0700

    Absolute IQR was being calculated incorrectly before. Also added kde_mode option'

--- analysis/helper_functions.py
+++ analysis/helper_functions.py
@@ -35,19 +35,7 @@ def distribution_values(data,weights,central,eff=1):
         return mu,mu_err,sigma,sigma_err,upper_quantile,lower_quantile
       if central == 'mean':
         return mean,mean_err,std,std_err
-      '''if central == 'mode':
-        Calculate mode using KDE:
-        bw = len(data)**(-1./5)  #scotts factor
-        kernel = gaussian_kde(data,weights=weights,bw_method=bw*2.0)
-        bins = numpy.histogram(data,weights=weights,bins=50)[1]
-        y = kernel(bins)
-        mode_est = bins[numpy.argmax(y)] 
-
-        binsize = bins[1]-bins[0]
-        smallbins = numpy.linspace(mode_est-2*binsize,mode_est+2*binsize,400) 
-        mode = smallbins[numpy.argmax(kernel(smallbins))] 
-        return mode,mean_err,std,std_err,kernel'''
-      if central == 'trimmed' or central == 'mode':
+      if central == 'trimmed' or central == 'mode' or central=='kde_mode':
         n,bins = numpy.histogram(data,weights=weights,bins=50)
         max_val = max(n) 
         max_bin = numpy.argmax(n)
@@ -82,21 +70,34 @@ def distribution_values(data,weights,central,eff=1):
         new_std_err = 0.5*new_var_err/newstd_est
 
         if central == 'trimmed': return newmean_est,new_mean_err,newstd_est,new_std_err,lower_val,upper_val 
-        #else central == 'mode': use absolute IQR
+        #else central == 'mode' or central=='kde_mode': use absolute IQR
+        if central == 'kde_mode':
+          #Calculate mode using KDE:
+          bw = len(data)**(-1./5)  #scotts factor
+          kernel = gaussian_kde(data,weights=weights,bw_method=bw*2.0)
+          bins = numpy.histogram(data,weights=weights,bins=50)[1]
+          y = kernel(bins)
+          mode_est = bins[numpy.argmax(y)] 
 
-        mu = newmean_est
-        mu_quantile = sum(weights[data<mu]) #should be approximately 0.5       
+          binsize = bins[1]-bins[0]
+          smallbins = numpy.linspace(mode_est-2*binsize,mode_est+2*binsize,400) 
+          mode = smallbins[numpy.argmax(kernel(smallbins))] 
+          mu = mode
+
+        if central == 'mode': mu = newmean_est
+        mu_quantile = sum(weights[data<mu]) 
+        absolute_mu_quantile = mu_quantile*eff+(1-eff) #should be approximately 0.5
         err = False
-        if abs(mu_quantile-0.5) > 0.1:
+        if abs(absolute_mu_quantile-0.5) > 0.1:
           print '<< Fitted mode is > .1 away from 50th percentile. Efficiency might be less than 50%. Calibration value might not be very useful. >>'
           err = True
-        if mu_quantile > 1-.3413:
+        if absolute_mu_quantile > 1-.3413:
           print '<< Fitted mode is at > 65th percentile! Bad fit. Efficiency might be less than 50%. Returning max value - mode. >>'
           upper_quantile = max(data)
           err = True
-        else: upper_quantile = quantile(data,weights,(mu_quantile+0.3413-(1-eff))/eff) #CDF(1)
-        if mu_quantile-0.3413 < (1-eff): lower_quantile = float('-inf')
-        else: lower_quantile = quantile(data,weights,(mu_quantile-.3413-(1-eff))/eff)
+        else: upper_quantile = quantile(data,weights,mu_quantile+.3413/eff) # = (absolute_mu_quantile+0.3413-(1-eff))/eff
+        if absolute_mu_quantile<0.3413+1-eff: lower_quantile = float('-inf')
+        else: lower_quantile = quantile(data,weights,mu_quantile-.3413/eff) # = (absolute_mu_quantile-0.3413-(1-eff))/eff
         sigma = (upper_quantile-mu)
         sigma_err = 1.573*std_err #http://stats.stackexchange.com/questions/110902/error-on-interquartile-range seems reasonable
         return mu,new_mean_err,sigma,sigma_err,upper_quantile,lower_quantile,err
