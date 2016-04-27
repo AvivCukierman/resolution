98a8af6fcedb265ca5a3743ee664c72bc7ee14e5
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-04-26 17:43:13 -0700

    Now makes eff vs. NPV plots

--- analysis/compare_eff.py
+++ analysis/compare_eff.py
@@ -41,8 +41,7 @@ def readCollections():
   return data
 
 import pickle
-def plot_sigmas():
-  collections_list = readCollections()
+def plot_eff_npv(collections_list):
   identifier = collections_list[0]['identifier']
   ptedges = pickle.load(open(options.submitDir+'/'+'ptedges_'+identifier+'.p','rb')) #assumes all the collections have the same ptedges
   for i,ptbin in enumerate(ptedges):
@@ -82,4 +81,55 @@ def plot_sigmas():
     plt.savefig(options.plotDir+'/jetefficiency_NPV_pt'+str(ptedges[i-1])+str(ptedges[i])+'_'+options.collections+'.png')
     plt.close()
 
-plot_sigmas()
+def plot_eff_pt(collections_list):
+  identifier = collections_list[0]['identifier']
+  ptedges = pickle.load(open(options.submitDir+'/'+'ptedges_'+identifier+'.p','rb')) #assumes all the collections have the same ptedges
+  ptedges.sort()
+  ptbin = ptedges[1]-ptedges[0]
+  npv_effs = pickle.load(open(options.submitDir+'/'+'efficiencies_'+identifier+'.p','rb')) #assumes all algorithms have the same NPV range
+  npv_keys = npv_effs.keys() 
+  npv_keys.sort()
+  npvbin = npv_keys[1]-npv_keys[0]
+  lowlim = {npv:float('inf') for npv in npv_keys}
+  for c in collections_list:
+    identifier = c['identifier']
+    avgpt = pickle.load(open(options.submitDir+'/'+'avgpttrue_'+identifier+'.p','rb')) #assumes all algorithms have the same avg pT true
+    npv_effs = pickle.load(open(options.submitDir+'/'+'efficiencies_'+identifier+'.p','rb')) #assumes all algorithms have the same NPV range
+    npv_eff_errs = pickle.load(open(options.submitDir+'/'+'efficiency_errs_'+identifier+'.p','rb'))
+
+    for i,npv in enumerate(npv_keys):
+      plt.figure(i)
+      plt.errorbar(avgpt,npv_effs[npv],yerr=npv_eff_errs[npv],color=c['color'],linestyle=c['ls'],label=c['label'])
+      lowlim[npv] = min(lowlim[npv],min(npv_effs[npv]))
+  #ATLAS style
+  for i,npv in enumerate(npv_keys):
+    plt.figure(i)
+    axes = plt.axes()
+    if atlas_style:
+      axes.xaxis.set_minor_locator(AutoMinorLocator())
+      axes.yaxis.set_minor_locator(AutoMinorLocator())
+      plt.xlabel('$p_T^{true}$ [GeV]', position=(1., 0.), va='bottom', ha='right')
+      plt.ylabel('Reconstruction Efficiency', position=(0., 1.), va='top', ha='right')
+      axes.xaxis.set_label_coords(1., -0.15)
+      axes.yaxis.set_label_coords(-0.15, 1.)
+      axes.text(0.05,0.9,'ATLAS', transform=axes.transAxes,size='larger',weight='bold',style='oblique')
+      axes.text(0.18,0.9,'Simulation', transform=axes.transAxes,size='larger')
+      axes.text(0.05,0.65,'$\mathregular{\sqrt{s}=13}$ TeV, $\mathregular{\mu=40}$\nPythia8 dijets\n'+str(npv-npvbin)+' < NPV < '+str(npv), transform=axes.transAxes,linespacing=1.5,size='larger')
+    else:
+      plt.errorbar([0],[0],linestyle=' ',label=str(npv-npvbin)+' < NPV < '+str(npv))
+      plt.xlabel('$p_T^{true}$ [GeV]')
+      plt.ylabel('Reconstruction Efficiency')
+    plt.ylim(lowlim[npv]-0.1,1.2)
+    plt.xlim(min(ptedges),max(ptedges))
+    # legend without errors: 
+    handles, labels = axes.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+    plt.legend(handles,labels,loc='upper right',frameon=False,numpoints=1,prop={'size':14})
+    plt.savefig(options.plotDir+'/jetefficiency_pt_NPV'+str(npv-npvbin)+str(npv)+'_'+options.collections+'.png')
+    plt.savefig(options.plotDir+'/jetefficiency_pt_NPV'+str(npv-npvbin)+str(npv)+'_'+options.collections+'.pdf')
+    plt.close()
+
+
+collections_list = readCollections()
+#plot_eff_npv(collections_list)
+plot_eff_pt(collections_list)
