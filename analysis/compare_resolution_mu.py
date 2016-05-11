8ea94abed5d17fff5a30588ce7295366490f51d1
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-05-10 18:23:44 -0700

    Only search in collections list O(1) times, not O(n) times.

--- analysis/compare_resolution_mu.py
+++ analysis/compare_resolution_mu.py
@@ -49,6 +49,17 @@ def plot_sigma_mu(collections_list,mus):
   identifier = collections_list[0]['identifiers'][0]
   ptedges = pickle.load(open(options.submitDir+'/'+'ptedges_'+identifier+'.p','rb')) #assumes all the collections have the same ptedges
 
+  incl_sigmas_mu = []
+  incl_sigma_errs_mu = []
+  for c in collections_list:
+    incl_sigmas = []
+    incl_sigma_errs = []
+    for identifier in c['identifiers']:
+      incl_sigmas.append(pickle.load(open(options.submitDir+'/'+'incl_sigmas_'+identifier+'.p','rb')))
+      incl_sigma_errs.append(pickle.load(open(options.submitDir+'/'+'incl_sigma_errs_'+identifier+'.p','rb')))
+    incl_sigmas_mu.append(incl_sigmas)
+    incl_sigma_errs_mu.append(incl_sigma_errs)
+
   for i,ptbin in enumerate(ptedges):
     if i==0: continue
 
@@ -57,12 +68,9 @@ def plot_sigma_mu(collections_list,mus):
     npv_sigmas = []
     npv_sigma_errs = []
 
-    for c in collections_list:
-      incl_sigmas = []
-      incl_sigma_errs = []
-      for identifier in c['identifiers']:
-        incl_sigmas.append(pickle.load(open(options.submitDir+'/'+'incl_sigmas_'+identifier+'.p','rb'))[i-1])
-        incl_sigma_errs.append(pickle.load(open(options.submitDir+'/'+'incl_sigma_errs_'+identifier+'.p','rb'))[i-1])
+    for j,c in enumerate(collections_list):
+      incl_sigmas = [sigmas[i-1] for sigmas in incl_sigmas_mu[j]]
+      incl_sigma_errs = [sigma_errs[i-1] for sigma_errs in incl_sigma_errs_mu[j]]
 
       plt.errorbar(mus,incl_sigmas,yerr=incl_sigma_errs,color=c['color'],linestyle=c['ls'],label=c['label'])
       lowlim = min(lowlim,min(incl_sigmas))
@@ -84,7 +92,7 @@ def plot_sigma_mu(collections_list,mus):
       plt.xlabel(options.mu_label)
       plt.ylabel('$\sigma[p_T^{reco}]$ [GeV]')
     plt.ylim(lowlim-0.5,highlim+2)
-    #plt.xlim(min(mus),max(mus))
+    plt.xlim(0,max(mus)+min(mus))
     # legend without errors: 
     handles, labels = axes.get_legend_handles_labels()
     handles = [h[0] for h in handles]
@@ -93,15 +101,28 @@ def plot_sigma_mu(collections_list,mus):
     plt.savefig(options.plotDir+'/jetsigma_mu_pt'+str(ptedges[i-1])+str(ptedges[i])+'_'+options.collections+'.pdf')
     plt.close()
 
+  incl_sigmas_mu = []
+  incl_sigma_errs_mu = []
+  for c in collections_list:
+    incl_sigmas = []
+    incl_sigma_errs = []
+    for identifier in c['identifiers']:
+      incl_sigmas.append(pickle.load(open(options.submitDir+'/'+'incl_sigmaRs_'+identifier+'.p','rb')))
+      incl_sigma_errs.append(pickle.load(open(options.submitDir+'/'+'incl_sigmaR_errs_'+identifier+'.p','rb')))
+    incl_sigmas_mu.append(incl_sigmas)
+    incl_sigma_errs_mu.append(incl_sigma_errs)
+
+  for i,ptbin in enumerate(ptedges):
+    if i==0: continue
+
     lowlim = float('inf')
     highlim = float('-inf')
+    npv_sigmas = []
+    npv_sigma_errs = []
 
-    for c in collections_list:
-      incl_sigmas = []
-      incl_sigma_errs = []
-      for identifier in c['identifiers']:
-        incl_sigmas.append(pickle.load(open(options.submitDir+'/'+'incl_sigmaRs_'+identifier+'.p','rb'))[i-1])
-        incl_sigma_errs.append(pickle.load(open(options.submitDir+'/'+'incl_sigmaR_errs_'+identifier+'.p','rb'))[i-1])
+    for j,c in enumerate(collections_list):
+      incl_sigmas = [sigmas[i-1] for sigmas in incl_sigmas_mu[j]]
+      incl_sigma_errs = [sigma_errs[i-1] for sigma_errs in incl_sigma_errs_mu[j]]
 
       plt.errorbar(mus,incl_sigmas,yerr=incl_sigma_errs,color=c['color'],linestyle=c['ls'],label=c['label'])
       lowlim = min(lowlim,min(incl_sigmas))
@@ -123,7 +144,7 @@ def plot_sigma_mu(collections_list,mus):
       plt.xlabel(options.mu_label)
       plt.ylabel('$\sigma[p_T^{reco}/p_T^{true}]$')
     plt.ylim(lowlim-0.05,highlim+.1)
-    #plt.xlim(min(mus),max(mus))
+    plt.xlim(0,max(mus)+min(mus))
     # legend without errors: 
     handles, labels = axes.get_legend_handles_labels()
     handles = [h[0] for h in handles]
